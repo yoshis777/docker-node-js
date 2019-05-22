1d9a40a1457c9ad2f5b9dee6ce6ee53e7105fabf
yoshis777 <alt0prom@gmail.com>
2019-05-23 08:15:11 +0900

    株価をDBに格納できるようにした。

---
+++ practice/get_stock_price_to_db.js
@@ -0,0 +1,74 @@
+const puppeteer = require('puppeteer');
+const stringify = require('csv-stringify');
+const iconv = require('iconv-lite');
+const fs = require('fs');
+const path = require('path');
+let { Client } = require('pg');
+require('dotenv').config();
+
+const createClientToDb = () => {
+    let client = new Client({
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASS,
+        port: process.env.DB_PORT
+    });
+    client.connect();
+
+    return client;
+};
+
+(async() => {
+    const browser = await puppeteer.launch({
+        headless: true,
+        slowMo: 50
+    });
+
+    const page = await browser.newPage();
+
+    await page.setViewport({
+        width: 1200,
+        height: 800
+    });
+
+    const stockCode = 6670;
+
+    await page.goto(`https://www.nikkei.com/nkd/company/?scode=${stockCode}`);
+
+    //銘柄
+    const stockName = await page.evaluate(() =>
+        document.querySelector('h1.m-headlineLarge_text').textContent
+    );
+
+    //株価
+    let stockPrice = await page.evaluate(() =>
+        document.querySelector('.m-stockPriceElm_value.now').textContent
+    );
+
+    //結果の取得
+    stockPrice = stockPrice.replace(/[^0-9]/g, '');
+    console.log(`銘柄コード ${stockCode} (${stockName}) の株価は ${stockPrice} です。`);
+
+    //DB接続
+    let client = createClientToDb();
+
+    const query = {
+        text: 'INSERT INTO "public"."tests" ("code", "name", "price") VALUES ($1, $2, $3)',
+        values: [stockCode, stockName, stockPrice]
+    };
+
+    client.query(query)
+        .then(res => {
+            console.log(res);
+            client.end(console.log('Closed client connection'));
+        })
+        .catch(err => {
+            console.error(err.stack);
+            client.end(console.log('Closed client connection'));
+        });
+
+    browser.close();
+
+})();
+
